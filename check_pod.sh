5119aafcbfdeb1ba9b7a9e903ef977ef1cbd91f6
daneshrao <38915899+daneshrao@users.noreply.github.com>
2020-07-13 12:01:20 +0530

    updated

--- check_pod.sh
+++ check_pod.sh
@@ -2,7 +2,7 @@
 #source ~/.bashrc
 #kubectl -n default patch hpa rails-app --patch '{"spec":{"maxReplicas":1}}'
 sleep 30
-pod_id=$(kubectl get pods|grep rails-app|awk '{if($2 == "0/1"||$3 == "Running"||$3 == "ContainerCreating")print $1}'|head -n 1)
+pod_id=$(kubectl get pods|grep rails-app|awk '{if($2 == "0/1"||$3 == "Pending"||$3 == "ContainerCreating")print $1}'|head -n 1)
 echo $pod_id
 while true; do
 status=$(kubectl get pods|grep "$pod_id"|awk '{if($2 == "0/1" && $3 == "Error" || $3 == "CrashLoopBackOff"|| $3 == "ImagePullBackOff")print $3}')
